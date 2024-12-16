afdfcc8c1786efc5d9ddeab70ff5f6440bbd8d5c
Chinnanavar <142550558+Chinnanavar@users.noreply.github.com>
2024-12-16 13:14:44 +0000

    conversion of datatypes

---
+++ .devcontainer/JS/03_conversionOperation.js
@@ -0,0 +1,32 @@
+// let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); //NaN Not a Number
+
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1; false => 0
+
+// let isLoggedIn = "yash"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "yash" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+
