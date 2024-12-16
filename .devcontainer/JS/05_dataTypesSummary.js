587a869a0ca49f52db9d452db54f38b7bf12fc00
Chinnanavar <142550558+Chinnanavar@users.noreply.github.com>
2024-12-16 14:55:29 +0000

    data type summary

---
+++ .devcontainer/JS/05_dataTypesSummary.js
@@ -0,0 +1,55 @@
+// Primitive / Value Type
+
+// 7 types : String, Number, Boolean, Null, Undefiend, Symbol, BigInt 
+
+const score = 100
+
+const scoreValue = 100.3
+
+const isLoggedIn = false
+
+const outsideTemp = null
+
+let userEmail = undefined;
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+
+// console.log(id === anotherid);
+
+// const bigNumber = 8429472389472837487329847238748234892378327482
+// const bigNum = 8429472389472837487329847238748234892378327482n
+// console.log(bigNumber);
+// console.log(bigNum);
+
+
+// console.log(id);
+// console.log(anotherid);
+
+
+
+
+// Non Primitivev / Refrence Type
+
+// Array, Objects, Function
+
+// const heros = ["shaktiman", "naagrag", "doga"]
+
+// const info = {
+//     name: "yash",
+//     age: 23,
+
+// }
+// console.log(heros);
+// console.log(typeof heros);
+
+// console.log(info);
+// console.log(typeof info);
+
+const myFunction = function() {
+    console.log("hello world");
+    
+}
+
+console.log(myFunction);
+
